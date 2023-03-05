9c5a706328a1740017728363705f40a0e027b565
Eyasmin Basher <eyasminbasher@gmail.com>
2023-03-05 23:12:57 +0000

    exercise 17 added

--- account.py
+++ account.py
@@ -5,14 +5,17 @@ class Account:
 
     def deposit(self, amount):  # deposit is a method!
         self._balance += amount
+        return f"You have deposited £{amount}."
 
     def get_balance(self):  # getter
         return self._balance
 
     def withdraw(self, amount):
-        # if amount > 0 and self._balance - amount >= 0:
-        # can also be written with a conditional expression: if self.balance >= amount > 0:
-        self._balance -= amount
+        if amount > 0 and self._balance - amount >= 0:
+            # can also be written with a conditional expression: if self.balance >= amount > 0:
+            try:
+                self._balance -= amount
+                print(f"You have withdrawn £{amount}.")
 
-
-
+            except InsufficientFunds as ex:
+                print(f"You have insufficient funds.")
--- bankapp.py
+++ bankapp.py
@@ -15,18 +15,16 @@ class Savings(Account):
 
 mirfat_current = Current(1000)
 mirfat_savings = Savings(200)
+print(f"Mirfat's current: £{mirfat_current.get_balance()}, Mirfat's savings: £{mirfat_savings.get_balance()}")
 
 pam_current = Current(500)
 pam_savings = Savings(100)
+print(f"Pam's current: £{pam_current.get_balance()}, Pam's savings: £{pam_savings.get_balance()}")
 
 eyasmin_current = Current(300)
 eyasmin_savings = Savings(400)
-
-print(f"Mirfat's current account's starting balance is £{mirfat_current.get_balance()}.")
+print(f"Eyasmin's current: £{eyasmin_current.get_balance()}, Eyasmin's savings: £{eyasmin_savings.get_balance()}")
 
 pam_savings.deposit(150)
-# how to add the deposit amount in the print statement?
-print(f"Pam's savings balance is now £{pam_savings.get_balance()}.")
 
-eyasmin_current.withdraw(-100)
-print(f"Eyasmin's current account's balance is £{eyasmin_current.get_balance()}.")
+eyasmin_current.withdraw(-500)
